0199f2688e896fd2005c68ced012258cc8cbf17d
Dougal Toms <dougal.toms@beazley.com>
2023-03-17 11:13:37 +0000

    Packaging

--- Python/setup.py
+++ Python/setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup
-
-setup(
-   name='wetherspoons',
-   version='1.0',
-   description='Boozy cocktail colour palettes courtesy of JD Wetherspoon',
-   author='Dougal Toms',
-   author_email='dougal.toms@gmail.com',
-   packages=['wetherspoons'],
-   install_requires=['Color', 'matplotlib']
+import setuptools
+  
+setuptools.setup(
+    name="wetherspoons",
+    version="0.0.1",
+    author="Dougal Toms",
+    author_email="dougal.toms@gmail.com",
+    packages=["wetherspoons"],
+    description="Colormaps inspired by the cocktails of JD Wetherspoon",
+    url="https://github.com/dougaltoms/wetherspoons",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=["matplotlib", "colours"]
 )
